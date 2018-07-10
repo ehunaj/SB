af0f964e7410f6d566249f386c83d56270d01e0e
Sarehun <30372603+Sarehu@users.noreply.github.com>
2018-07-10 07:37:43 +0700

    Update server.py

--- linepy/server.py
+++ linepy/server.py
@@ -26,9 +26,10 @@ class LineServer(object):
         'LINE_MUSIC': '1381425814'
     }
 
-    USER_AGENT  = 'Line/7.18.1'
+    USER_AGENT  = 'Line/1.7.14' #7.18.1'
     APP_TYPE    = ApplicationType.IOS
-    APP_NAME    = 'IOSIPAD\t7.18.1\tiPhone OS\t11.2.5'
+    APP_NAME    = 'CHROMEOS\t1.4.17\tChrome_OS\t1'
+    #APP_NAME    = 'IOSIPAD\t7.18.1\tiPhone OS\t11.2.5'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'FDLRCN'
     IP_ADDR     = '8.8.8.8'
